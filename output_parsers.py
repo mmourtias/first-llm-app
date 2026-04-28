49944635a251de7457e8eb1178a49021688583aa
mmourtias <mmourtias2002@gmail.com>
2026-04-28 15:47:32 +0300

    add output parsers

---
+++ output_parsers.py
@@ -0,0 +1,28 @@
+from langchain_groq import ChatGroq 
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+load_dotenv()
+
+client = ChatGroq(
+    model="llama-3.3-70b-versatile"
+) 
+
+
+
+parser = StrOutputParser()
+
+template = ChatPromptTemplate.from_messages([
+    ("system", "Είσαι ένας ειδικός σε {subject}. Απαντάς πάντα ΜΟΝΟ στα ελληνικά."),
+    ("human", "{question}")
+])
+
+chain = template | client | parser
+
+result = chain.invoke({"subject": "Python programming",
+    "question": "Ποιες είναι οι 3 πιο χρήσιμες built-in functions;"
+})
+
+print(result)
+print(type(result))
